b15ed48e73b7ee64f8eca5a8df7486c9b8da1539
Dilan <tdilan@uninorte.edu.co>
2020-03-13 18:49:37 -0500

    Comentarios Eliminados

--- Game/src/com/Domain/prototype/Game.java
+++ Game/src/com/Domain/prototype/Game.java
@@ -15,89 +15,80 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
 
+public class Game extends Canvas implements Runnable {
 
-
-public class Game extends Canvas implements Runnable{
-    private static final long serialVersionUID=1L;
+    private static final long serialVersionUID = 1L;
     public static int width = 300;
-    public static int height = width/16*9;
+    public static int height = width / 16 * 9;
     public static int scale = 3;
-    public static int i=0;
+    public static int i = 0;
     int x;
     int y;
-    
-    
-    
+
     private Thread thread;
-    private boolean running=true;
+    private boolean running = true;
     JFrame frame;
 
-    
-        public Game(int x, int y){
-            Dimension size = new Dimension(width * scale, height*scale);
-            setPreferredSize(size);
-            frame= new JFrame();
-            this.x=x;
-            this.y=y;
+    public Game(int x, int y) {
+        Dimension size = new Dimension(width * scale, height * scale);
+        setPreferredSize(size);
+        frame = new JFrame();
+        this.x = x;
+        this.y = y;
+    }
+
+    public synchronized void start() {
+        thread = new Thread(this, "1");
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        try {
+            thread.join();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-    
-    //FIX IT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! please. :P
-        public synchronized void start(){
-            thread = new Thread(this, "1");
-            thread.start();
+    }
+
+    public void run() {
+        while (running) {
+            render();
         }
-        
-        public synchronized void stop(){
-            try{
-                thread.join();
-            
-            }catch(InterruptedException e){
-                e.printStackTrace();
-            }
+    }
+
+    public void render() {
+        Random colornum = new Random();
+        BufferStrategy bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+
         }
-        
-        public void run(){
-            while(running){
-              render();
-            }       
-        }
-        
-        public void render(){
-            Random colornum = new Random();
-            BufferStrategy bs = getBufferStrategy();
-            if(bs==null){
-                createBufferStrategy(3);
-                return;
-                
-            }
-             
-            Graphics g = bs.getDrawGraphics();
-            g.setColor(Color.BLACK);
-            
-            //Aquí
-            g.fillRect(0, 0, getWidth(), getHeight());//ventana de fondo 
-          
-            
-            
-            g.setColor(new Color(colornum.nextInt(100)+1,colornum.nextInt(100)+1,colornum.nextInt(100)+1));
-            
-            g.fillRect(x, y, 200, 200); //Rectangulo de colores
-            int xcoord[]= {50,100,180,0+i,50} ;//Vector de puntos en x
-            int ycoord[]={50,130,150,0+i,50};// Vector de puntos en y
-            g.setColor(Color.WHITE);
-              g.fillPolygon(xcoord,ycoord, 5/*Numero de puntos*/);
-            i+=3;     
+
+        Graphics g = bs.getDrawGraphics();
+        g.setColor(Color.BLACK);
+
+        //Aquí Empieza
+        g.fillRect(0, 0, getWidth(), getHeight());//ventana de fondo 
+        g.setColor(new Color(colornum.nextInt(100) + 1, colornum.nextInt(100) + 1, colornum.nextInt(100) + 1));
+        g.fillRect(x, y, 200, 200); //Rectangulo de colores
+        int xcoord[] = {50, 100, 180, 0 + i, 50};//Vector de puntos en x
+        int ycoord[] = {50, 130, 150, 0 + i, 50};// Vector de puntos en y
+        g.setColor(Color.WHITE);
+        g.fillPolygon(xcoord, ycoord, 5/*Numero de puntos*/);
+        i += 3;
         try {
             Thread.sleep(500);
         } catch (InterruptedException ex) {
             ex.printStackTrace();
         }
-        //Aquí
+        //Aquí Termina
         
-            //clear the frame
-            g.dispose();
-            //display the buffer
-            bs.show();
-            
-        }
+        //clear the frame
+        g.dispose();
+        //display the buffer
+        bs.show();
+
+    }
 }
--- Game/src/com/Domain/prototype/main.java
+++ Game/src/com/Domain/prototype/main.java
@@ -15,7 +15,7 @@ public class main {
     public static void main(String[]arg) throws InterruptedException{
         Game game = new Game(50,50);
         
-        //Game game2=new Game(100,100); Dos hilos no pueden trabajar con un mismo objeto 
+        
         
         //Mantiene las dimensiones establecidas en el objeto, y deshabilita al usuario para cambiar la misma
         game.frame.setResizable(false);
@@ -30,24 +30,13 @@ public class main {
         game.frame.setLocationRelativeTo(null);
         game.frame.setVisible(true);
         
-//        
-//        game2.frame.setResizable(false);
-//        game2.frame.setTitle("Hera's Legacy2");
-//        //genera como al objeto como componente del Frame (Gracias a la extensión Canvas)
-//        game2.frame.add(game);
-//        //Mantiene las dimensiones del componente game
-//        game2.frame.pack();
-//        //Cierra el proceso cuando cierras la ventana
-//        game2.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        //Centra la ventana
-//        game2.frame.setLocationRelativeTo(null);
-//        game2.frame.setVisible(true);
+
         
         game.start();
         for (int i = 0; i < 100; i++) {
             Thread.sleep(1000);
             System.out.println(i);
         }
-        //game2.start();
+        
     }
 }
