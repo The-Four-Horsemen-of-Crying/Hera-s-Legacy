a79e612d583b8b5e881dbd69f999066ad3106e14
Jonathan Arias <ariasej@uninorte.edu.co>
2020-05-08 18:56:45 -0500

    Clase Lobby creada

---
+++ Game/src/com/heraslegacy/level/Lobby.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.heraslegacy.level;
+
+/**
+ *
+ * @author jony1
+ */
+public class Lobby extends MathLevel{
+    
+}
