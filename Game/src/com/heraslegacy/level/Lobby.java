6181786c0e5a5e90d001e79569fca574aaba6eaf
Jonathan Arias <ariasej@uninorte.edu.co>
2020-05-08 19:13:53 -0500

    Metodo loadLevel implementado

--- Game/src/com/heraslegacy/level/Lobby.java
+++ Game/src/com/heraslegacy/level/Lobby.java
@@ -5,10 +5,71 @@
  */
 package com.heraslegacy.level;
 
+import com.heraslegacy.level.tile.Tile;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 /**
  *
  * @author jony1
  */
-public class Lobby extends MathLevel{
-    
+public class Lobby implements levelStrategy{
+ protected int width;
+    protected int height;
+    protected int[] tiles; 
+    protected int[] tilesCollision;
+    @Override
+    public void update() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Tile getTile(int x, int y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean getCollision(int x, int y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void loadLevel(String path, String pathCollision) {
+        try {
+            BufferedImage image = ImageIO.read(MathLevel.class.getResource(path));
+            BufferedImage imageCollision = ImageIO.read(MathLevel.class.getResource(pathCollision));
+            int w = width = image.getWidth();
+            int h = height = image.getHeight();
+            tiles = new int[w * h];
+            tilesCollision = new int[w * h];
+
+            image.getRGB(0, 0, w, h, tiles, 0, w);
+            imageCollision.getRGB(0, 0, w, h, tilesCollision, 0, w);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("No se pudo cargar el archivo del nivel.");
+        }
+    }
+
+    @Override
+    public void time() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mecanica() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void restar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean cambio() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+        
 }
