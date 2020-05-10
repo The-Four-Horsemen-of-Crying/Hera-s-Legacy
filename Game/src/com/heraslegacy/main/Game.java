69a59b3b37df3a7760eac8002dc7145488f27d83
Dilan <tdilan@uninorte.edu.co>
2020-05-09 19:00:53 -0500

    Maqueta LibraryLevel

--- Game/src/com/heraslegacy/main/Game.java
+++ Game/src/com/heraslegacy/main/Game.java
@@ -29,7 +29,6 @@ public class Game extends Canvas implements Runnable {
     private KeyBoard key;
     private Mouse mouse;
     public JFrame frame; 
-    
     private static final long serialVersionUID = 1L;
     public static int width = 300;
     public static int height = width / 16 * 9;
@@ -143,9 +142,9 @@ public class Game extends Canvas implements Runnable {
         }
         
         if(key.restart){
-            level01.getPlayer().setX(spawnpj[0]);
-            level01.getPlayer().setY(spawnpj[1]);
-            level01.restart();
+            level02.getPlayer().setX(spawnpj[0]);
+            level02.getPlayer().setY(spawnpj[1]);
+            level02.restart();
         }
         
         for (int i = 0; i < pixels.length; i++) {
